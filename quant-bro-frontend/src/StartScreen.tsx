68c67792d4d225aaf13771487e3c884729d22d91
xaizko <tonytran177013@gmail.com>
2025-11-02 07:32:58 -0500

    reworded front page

--- quant-bro-frontend/src/StartScreen.tsx
+++ quant-bro-frontend/src/StartScreen.tsx
@@ -147,8 +147,8 @@ function StartScreen() {
 	<>
 	    <div className="start-screen-container">
 		<div className="start-screen-content">
-		    <h1> Quant Bro Simulator </h1>
-		    <p> Welcome to the crypto trading simulator game! </p>
+		    <h1> Quant Bro </h1>
+		    <p> Welcome to the real-time crypto trading simulator game! </p>
 		    <p className="subtitle"> Powered by Solana </p>
 
 		    <input type="password"
